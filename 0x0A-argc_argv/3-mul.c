d278865467b682d76a5399256428cdaf99a6dae1
Natasha <reddynatasha23@gmail.com>
2023-04-13 06:48:17 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -57,14 +57,14 @@ int main(int argc, char *argv[])
 {
 	int result, num1, num2;
 
-	if (argc < 3 || argc 3)
+	if (argc < 3 || argc > 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
 
-	num1 = _atoi(argv[1];
-	num2 = _atoi(argv[2];
+	num1 = _atoi(argv[1]);
+	num2 = _atoi(argv[2]);
 	result = num1 * num2;
 
 	printf("%d\n", result);
